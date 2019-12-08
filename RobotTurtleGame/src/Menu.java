be97e2598babf6a9ad4c9b0b87dfc961a697dab8
AnthonyGiusti <58392616+AnthonyGiusti@users.noreply.github.com>
2019-12-08 15:11:02 +0100

    MaJ des imports

--- RobotTurtleGame/src/Menu.java
+++ RobotTurtleGame/src/Menu.java
@@ -1,20 +1,15 @@
-import java.awt.BorderLayout; 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.Font;
 import java.awt.GridLayout;
-import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
 
-import javax.imageio.ImageIO;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextArea;
 
 public class Menu extends JFrame{
 
