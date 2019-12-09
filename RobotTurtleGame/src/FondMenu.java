224b34ff7fc3e46e7896245c30e863050500c801
AnthonyGiusti <58392616+AnthonyGiusti@users.noreply.github.com>
2019-12-09 18:51:11 +0100

    Rename de la classe

---
+++ RobotTurtleGame/src/FondMenu.java
@@ -0,0 +1,20 @@
+import java.awt.Graphics;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+public class FondMenu extends JPanel {
+	
+  public void paintComponent(Graphics g){
+//  Image de fond
+	try {
+      Image bg = ImageIO.read(new File("backgroundAndLogo.png"));
+      g.drawImage(bg, 0, 0, this.getWidth(), this.getHeight(), this);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  	}
+}
