aff953d6d7d15e991065800c8fce2557fe3a6c56
MaxTL <58427275+MaxTL@users.noreply.github.com>
2019-12-12 17:41:11 +0100

    Update Questions.java

--- RobotTurtleGame/src/Questions.java
+++ RobotTurtleGame/src/Questions.java
@@ -3,9 +3,16 @@ public class Questions {
 /*
  * Comment faire un fichier README
  * 
- * Comment connecter Github pour les personnes qui ne sont pas créateurs du projet
+ * Comment connecter Github pour les personnes qui ne sont pas crÃ©ateurs du projet
  * 
+ * Doit-on mettre les constructeurs dans le diagramme de classe
  * 
- * 
+ * Comment on Pull (GitHub) sur IntelliJ
+ *
+ * Les conditions pour ajouter un mur
+ *
+ * Est ce qu'on peut faire comme un bataille navale pour le plateau (pour calculer les positions)
+ *
+ *Explication de comment se servir des branches 
  */
 }
