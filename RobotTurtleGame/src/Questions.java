f71975c970539a5d4d4aed51479104578dcabe23
AnthonyGiusti <58392616+AnthonyGiusti@users.noreply.github.com>
2019-12-15 11:16:32 +0100

    Ecrire ici toutes les questions qu'on peut avoir

--- RobotTurtleGame/src/Questions.java
+++ RobotTurtleGame/src/Questions.java
@@ -1,18 +1,20 @@
-
-public class Questions {
 /*
  * Comment faire un fichier README
+ *  
+ * Doit-on mettre les constructeurs dans le diagramme de classe		V
  * 
- * Comment connecter Github pour les personnes qui ne sont pas crÃ©ateurs du projet
- * 
- * Doit-on mettre les constructeurs dans le diagramme de classe
- * 
- * Comment on Pull (GitHub) sur IntelliJ
+ * Comment on Pull (GitHub) sur IntelliJ			V
  *
  * Les conditions pour ajouter un mur
  *
  * Est ce qu'on peut faire comme un bataille navale pour le plateau (pour calculer les positions)
  *
- *Explication de comment se servir des branches 
+ *Explication de comment se servir des branches 	V
+ *
+ *A quoi correspoint "this" à la fin de drawImage	V 
+ *
+ *Aide pour implémenter la classe tortue dans plateau
  */
+public class Questions {
+
 }
