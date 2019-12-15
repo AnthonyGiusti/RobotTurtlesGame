85bdbe154e7d14e19dac9ac1d3dc6faa846f32f5
AnthonyGiusti <58392616+AnthonyGiusti@users.noreply.github.com>
2019-12-15 11:19:09 +0100

    Je sais plus ce qui a changé

--- RobotTurtleGame/src/Plateau.java
+++ RobotTurtleGame/src/Plateau.java
@@ -7,37 +7,96 @@ import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 public class Plateau extends JPanel{
-    public int[][] plateau = new int[8][8];
-    public int[] positionJoueur1 = {1,5};
-    public int[] positionJoueur2 = {5,5};
+    private int[][] plateau = new int[8][8];
+    Tortue turtles = new Tortue();
+    private Image imagePlateau;
+    private Image imageRuby;
+    private Image imageMur;
+	private Image imageTortue;
+	private Image imageTortue2;
+    
+    public int[][] getPlateau() {
+		return plateau;
+	}
 
+	public void setPlateau(int[][] plateau) {
+		if(plateau.length < -1) {
+			System.out.println("Incorrect input");
+		}
+		this.plateau = plateau;
+	}
+
+	public Tortue getTurtles() {
+		return turtles;
+	}
+
+	public void setTurtles(Tortue turtles) {
+		this.turtles = turtles;
+	}
+
+	public Image getImage_plateau() {
+		return imagePlateau;
+	}
+
+	public void setImage_plateau(Image image_plateau) {
+		this.imagePlateau = image_plateau;
+	}
+
+	public Image getImage_ruby() {
+		return imageRuby;
+	}
+
+	public void setImage_ruby(Image image_ruby) {
+		this.imageRuby = image_ruby;
+	}
+
+	public Image getImage_wall() {
+		return imageMur;
+	}
+
+	public void setImage_wall(Image image_wall) {
+		this.imageMur = image_wall;
+	}
+
+	public Image getImage_tortue() {
+		return imageTortue;
+	}
+
+	public void setImage_tortue(Image image_tortue) {
+		this.imageTortue = image_tortue;
+	}
+
+	public Plateau(String image_plateau, String image_ruby, String image_wall, String image_tortue, String image_tortue2) {
+    	try {
+	    	this.imagePlateau = ImageIO.read(new File(image_plateau));
+	    	this.imageRuby = ImageIO.read(new File(image_ruby));
+	    	this.imageMur = ImageIO.read(new File(image_wall));
+	    	this.imageTortue = ImageIO.read(new File(image_tortue));
+    	} catch(IOException e) {
+    		e.printStackTrace();
+    	}
+    }
+    
     public void paintComponent(Graphics g){
-        try{
-            //Les tortues et le ruby
+        g.drawImage(this.imagePlateau, 0, 0,800, 800, this);
+        
+        
+//        g.drawImage(imageTortue, 100*positionJoueur1[0], 100*positionJoueur1[1], 95, 95,  this);
+//        g.drawImage(image, 98*positionJoueur2[0], 100*positionJoueur2[1], 95, 95,  this);
+        g.drawImage(this.imageRuby, 300, 700, 100, 100,  this);
 
-            Image plateau = ImageIO.read(new File("Plateau.png"));
-            Image turtle1 = ImageIO.read(new File("turtle1.png"));
-            Image turtle2 = ImageIO.read(new File("turtle2.png"));
-            Image ruby = ImageIO.read(new File("RUBY.png"));
-            Image wall = ImageIO.read(new File("WALL.png"));
-            g.drawImage(plateau, 0, 0,800, 800, this);
-            g.drawImage(turtle1, 100*positionJoueur1[0], 100*positionJoueur1[1], 95, 95,  this);
-            g.drawImage(turtle2, 98*positionJoueur2[0], 100*positionJoueur2[1], 95, 95,  this);
-            g.drawImage(ruby, 295, 660, 95, 95,  this);
+        //Les murs
+        int xMur = 700;
+        int widthMur = 100;        
+        int heightMur = 100;
+        g.drawImage(imageMur, xMur,   0, widthMur, heightMur,  this);
+        g.drawImage(imageMur, xMur,  100, widthMur, heightMur,  this);
+        g.drawImage(imageMur, xMur, 200, widthMur, heightMur,  this);
+        g.drawImage(imageMur, xMur, 300, widthMur, heightMur,  this);
+        g.drawImage(imageMur, xMur, 400, widthMur, heightMur,  this);
+        g.drawImage(imageMur, xMur, 500, widthMur, heightMur,  this);
+        g.drawImage(imageMur, xMur, 600, widthMur, heightMur,  this);
+        g.drawImage(imageMur, xMur, 700, widthMur, heightMur,  this);
 
-            //Les murs
-
-            g.drawImage(wall, 687,   0, 95, 95,  this);
-            g.drawImage(wall, 687,  95, 95, 95,  this);
-            g.drawImage(wall, 687, 190, 95, 95,  this);
-            g.drawImage(wall, 687, 285, 95, 95,  this);
-            g.drawImage(wall, 687, 380, 95, 95,  this);
-            g.drawImage(wall, 687, 475, 95, 95,  this);
-            g.drawImage(wall, 687, 570, 95, 95,  this);
-            g.drawImage(wall, 687, 665, 95, 95,  this);
-
-        } catch (IOException e){
-            e.printStackTrace();
-        }
     }
 }
---
+++ RobotTurtleGame/src/Tortue.java
@@ -0,0 +1,57 @@
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.ImageObserver;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class Tortue {
+
+	public int [] positionTortue1 = new int [2];
+	public int [] positionTortue2 = new int [2];
+	public int [] positionTortue3 = new int [2];
+	public int [] positionTortue4 = new int [2];
+	
+	public void paintComponent(Graphics g) {
+        try{
+        	if (Menu.nb_joueurs==2) {
+        		positionTortue1 = new int[] {1,0};
+        		positionTortue2 = new int[] {5,0};
+        		Image turtle1 = ImageIO.read(new File("turtle1.png"));
+        		Image turtle2 = ImageIO.read(new File("turtle2.png"));
+        		g.drawImage(turtle1, 100*positionTortue1[0], 100*positionTortue1[1], 95, 95, (ImageObserver) this);
+        		g.drawImage(turtle2, 98*positionTortue2[0], 100*positionTortue2[1], 95, 95,  (ImageObserver) this);
+        	}
+        	else if(Menu.nb_joueurs == 3) {
+        		positionTortue1 = new int[] {};
+        		positionTortue2 = new int[] {};
+        		positionTortue3 = new int[] {};
+        		Image turtle1 = ImageIO.read(new File("turtle1.png"));
+        		Image turtle2 = ImageIO.read(new File("turtle2.png"));
+        		Image turtle3 = ImageIO.read(new File("turtle3.png"));
+        		g.drawImage(turtle1, 100*positionTortue1[0], 100*positionTortue1[1], 95, 95, (ImageObserver) this);
+        		g.drawImage(turtle2, 98*positionTortue2[0], 100*positionTortue2[1], 95, 95,  (ImageObserver) this);
+        		g.drawImage(turtle3, 100*positionTortue3[0], 100*positionTortue3[1], 95, 95, (ImageObserver) this);
+        	}
+        	else if(Menu.nb_joueurs == 4) {
+        		positionTortue1 = new int[] {};
+        		positionTortue2 = new int[] {};
+        		positionTortue3 = new int[] {};
+        		positionTortue4 = new int[] {};
+        		Image turtle1 = ImageIO.read(new File("turtle1.png"));
+        		Image turtle2 = ImageIO.read(new File("turtle2.png"));
+        		Image turtle3 = ImageIO.read(new File("turtle3.png"));
+        		Image turtle4 = ImageIO.read(new File("turtle4.png"));
+        		g.drawImage(turtle1, 100*positionTortue1[0], 100*positionTortue1[1], 95, 95, (ImageObserver) this);
+        		g.drawImage(turtle2, 98*positionTortue2[0], 100*positionTortue2[1], 95, 95,  (ImageObserver) this);
+        		g.drawImage(turtle3, 100*positionTortue3[0], 100*positionTortue3[1], 95, 95, (ImageObserver) this);
+        		g.drawImage(turtle4, 100*positionTortue4[0], 100*positionTortue4[1], 95, 95, (ImageObserver) this);
+
+        		System.out.println("erreur pour l'instant");
+        	}
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+	}
+}
