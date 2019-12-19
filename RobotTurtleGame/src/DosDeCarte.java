b63ae6036c0a47c0aa3f1d225b520749e9b276ff
AnthonyGiusti <58392616+AnthonyGiusti@users.noreply.github.com>
2019-12-19 19:10:32 +0100

    Bah lisez si vous voulez comprendre, je sais pas comment expliquer mdr

--- RobotTurtleGame/src/DosDeCarte.java
+++ RobotTurtleGame/src/DosDeCarte.java
@@ -6,14 +6,32 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
+@SuppressWarnings("serial")
 public class DosDeCarte extends JPanel{
-	public void paintComponent(Graphics g) {
-		Image img;
+	private Image dosCarte;
+	private int width = 75;
+	private int height = 100;
+	
+	public DosDeCarte(String nom_du_fichier) {
 		try {
-			 img = ImageIO.read(new File(""));
-		      g.drawImage(img, 0, 0, this);
+			this.dosCarte = ImageIO.read(new File(nom_du_fichier));
 		}catch(IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public void paintComponent(Graphics g) {
+		if(Menu.nb_joueurs == 2) {
+			g.drawImage(this.dosCarte, 125, 125, width, height, (JPanel) this);
+
+		}
+		if(Menu.nb_joueurs == 3) {
+			g.drawImage(this.dosCarte, 125, 75, width, height, (JPanel) this);
+
+		}
+		if(Menu.nb_joueurs == 4) {
+			g.drawImage(this.dosCarte, 125, 25, width, height, (JPanel) this);
+		}
+			
 	}
 }
